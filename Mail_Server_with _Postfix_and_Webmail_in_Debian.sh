23ad843a312ec6ad4171dd70ece4a4d40197c6e2
maysara <maysarax@users.noreply.github.com>
2019-02-07 16:14:15 +0200

    Create Mail_Server_with _Postfix_and_Webmail_in_Debian.sh

---
+++ Mail_Server_with _Postfix_and_Webmail_in_Debian.sh
@@ -0,0 +1,12 @@
+#  Step 1: Initial Configurations for Postfix Mail Server on Debian
+
+
+# 1.In the first step, login to your machine with an account with root privileges or directly with the root user and make sure your Debian system is up to date with the latest security patches and software and packages releases, by issuing the following command.
+
+apt-get update 
+
+apt-get upgrade 
+
+#2. On the next step install the following software packages that will be used for system administration, by issuing the following command.
+
+apt-get install curl net-tools bash-completion wget lsof nano
